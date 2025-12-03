2d1d14478325e164e21058210dd498d41e52200d
dlettman <derek.lettman@gmail.com>
2025-12-02 21:40:49 -0800

    solve day 3

--- 03/exercise_03.py
+++ 03/exercise_03.py
@@ -5,18 +5,27 @@ import pyperclip
 from helpers import helpers
 
 
+def get_joltage(line, num_batteries=12):
+    last_idx = 0
+    num_so_far = ""
+    for n in range(num_batteries):
+        best_so_far, best_idx = 0, 0
+        for idx, char in enumerate(line[last_idx:((len(line) + 1)- (num_batteries-n))]):
+            if int(char) > best_so_far:
+                best_so_far = int(char)
+                best_idx = idx
+        last_idx = best_idx + last_idx + 1
+        num_so_far += str(best_so_far)
+    return int(num_so_far)
+
 def part_one(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    return sum([get_joltage(line, num_batteries=2) for line in puzzle_input])
 
 
 def part_two(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    return sum([get_joltage(line) for line in puzzle_input])
 
 
 if __name__ == "__main__":
