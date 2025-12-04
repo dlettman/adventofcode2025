f1b2c66eb0512c197cd5912c2d1864976a03624f
dlettman <derek.lettman@gmail.com>
2025-12-03 21:17:55 -0800

    solve day 4

--- 04/exercise_04.py
+++ 04/exercise_04.py
@@ -4,19 +4,55 @@ import pyperclip
 
 from helpers import helpers
 
+def get_grid(puzzle_input):
+    paper = set()
+    for y, row in enumerate(puzzle_input):
+        for x, char in enumerate(row):
+            if  char == "@":
+                paper.add((x, y))
+    return paper
+
 
 def part_one(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    paper = get_grid(puzzle_input)
+    total_accessible = 0
+    for y, row in enumerate(puzzle_input):
+        for x, char in enumerate(row):
+            paper_pals = 0
+            if (x, y) in paper:
+                for neighbor in helpers.NEIGHBORS:
+                    neighbor_coord = (x + neighbor[0], y + neighbor[1])
+                    if  neighbor_coord in paper:
+                        paper_pals += 1
+                if paper_pals < 4:
+                    total_accessible += 1
+    return total_accessible
 
 
 def part_two(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    paper = get_grid(puzzle_input)
+    moved_some = True
+    total_removed = 0
+    while moved_some:
+        moved_some = False
+        new_set = set()
+        for y, row in enumerate(puzzle_input):
+            for x, char in enumerate(row):
+                paper_pals = 0
+                if (x, y) in paper:
+                    for neighbor in helpers.NEIGHBORS:
+                        neighbor_coord = (x + neighbor[0], y + neighbor[1])
+                        if neighbor_coord in paper:
+                            paper_pals += 1
+                    if paper_pals < 4:
+                        total_removed += 1
+                        moved_some = True
+                    else:
+                        new_set.add((x, y))
+        paper = new_set
+    return total_removed
 
 
 if __name__ == "__main__":
